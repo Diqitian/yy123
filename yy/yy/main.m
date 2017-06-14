bfe32709f6efb5d6783e4f589a2e932cc54a8817
yhg <702455955@qq.com>
2017-06-14 19:04:59 +0800

    Ccc

---
+++ yy/yy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yy
+//
+//  Created by EDZ on 2017/6/14.
+//  Copyright © 2017年 meihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
